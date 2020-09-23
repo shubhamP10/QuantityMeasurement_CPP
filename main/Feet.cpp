866895ef03b340a5f877ad1a2dad9186ffee5bd0
“Shubham” <shubhampattar2@gmail.com>
2020-09-23 15:18:46 +0530

    REFACTORED code and test cases for logic.

--- main/Feet.cpp
+++ main/Feet.cpp
@@ -4,10 +4,19 @@ Feet::Feet(double value) {
     this->value = value;
 }
 
-bool Feet::operator == (Feet other) const {
-    return (this->value == other.value); //value check
+bool Feet::operator == (Feet *other) const {
+    if(other == nullptr || this == nullptr) { //null check
+        return false;
+    }
+
+    if(this == other) { //reference check
+        return true;
+    }
+
+    return (this->value == other->value); //value check
 }
 
-bool Feet::operator != (Feet* other) const {
-    return !(this == other);
+bool Feet::operator == (Feet other) const {
+
+    return (this->value == other.value); //value check
 }
--- test/QuantityMeasurementTest.cpp
+++ test/QuantityMeasurementTest.cpp
@@ -4,20 +4,30 @@
 //1.1
 TEST(FeetTests, given0FeetAnd0Feet_ShouldReturnEqual) { 
     Feet first_zero_feet(0), second_zero_feet(0);  
-    ASSERT_EQ(first_zero_feet, second_zero_feet);
+    bool result = (first_zero_feet == second_zero_feet);
+    ASSERT_TRUE(result);
 }
 
 //1.2
 TEST(FeetTests, givenNullAnd1Feet_ShouldNotEqual) { 
     Feet first_zero_feet(1);
-    ASSERT_NE(first_zero_feet, nullptr);
+    bool result = (first_zero_feet == nullptr);
+    ASSERT_FALSE(result);
 }
 
 //1.3
 TEST(FeetTests, given1ObjectAnd1References_ShouldBeEqual) { 
     Feet *first_ref = new Feet(1);
     Feet *second_ref = first_ref;
-    ASSERT_EQ(first_ref, second_ref);
+    bool result(first_ref == second_ref);
+    ASSERT_TRUE(result);
+}
+
+//1.4
+TEST(FeetTests, given1FeetAnd1Feet_ShouldReturnEqual) {
+    Feet feet1(1), feet2(1);
+    bool result = (feet1 == feet2);
+    ASSERT_TRUE(result);
 }
 
 int main(int argc, char **argv) {
